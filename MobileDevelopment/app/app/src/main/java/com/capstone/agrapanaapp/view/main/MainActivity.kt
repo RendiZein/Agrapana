2fe956819a343bd8ac4aae189c3837d352cb92c9
Syn57 <luthfi.n57@gmail.com>
2022-05-27 18:27:21 +0700

    Creating input edit text in login activity

--- MobileDevelopment/app/app/src/main/java/com/capstone/agrapanaapp/view/main/MainActivity.kt
+++ MobileDevelopment/app/app/src/main/java/com/capstone/agrapanaapp/view/main/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.capstone.agrapanaapp.view.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.capstone.agrapanaapp.R
+import com.capstone.agrapanaapp.view.authentication.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val i = Intent(this@MainActivity, LoginActivity::class.java)
+        startActivity(i)
+        supportActionBar?.hide()
     }
 }
