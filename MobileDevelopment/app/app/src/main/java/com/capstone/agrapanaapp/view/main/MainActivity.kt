e505ba237108a33e1faf5d82ab695047fcadb940
Syn57 <luthfi.n57@gmail.com>
2022-05-28 12:04:29 +0700

    fixing btn camera

--- MobileDevelopment/app/app/src/main/java/com/capstone/agrapanaapp/view/main/MainActivity.kt
+++ MobileDevelopment/app/app/src/main/java/com/capstone/agrapanaapp/view/main/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CancellationSignal
+import android.util.Log
 import com.capstone.agrapanaapp.R
 import com.capstone.agrapanaapp.databinding.ActivityMainBinding
 import com.capstone.agrapanaapp.view.authentication.login.LoginActivity
@@ -15,13 +16,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val i = Intent(this@MainActivity, LoginActivity::class.java)
-        startActivity(i)
+
+//        val i = Intent(this@MainActivity, LoginActivity::class.java)
+//        startActivity(i)
         supportActionBar?.hide()
         binding = ActivityMainBinding.inflate(layoutInflater)
-
+        setContentView(binding.root)
         binding.btnCamera.setOnClickListener {
+            Log.d("TAG", "onCreate: button clicked")
             startActivity(Intent(this@MainActivity, CameraActivity::class.java))
         }
 
