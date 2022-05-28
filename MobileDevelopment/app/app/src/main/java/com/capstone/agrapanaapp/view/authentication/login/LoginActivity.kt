31f94c3af576bb8b72384bab4459ee25ed390577
Syn57 <luthfi.n57@gmail.com>
2022-05-28 20:31:45 +0700

    fixing some error in activity_register.xml

--- MobileDevelopment/app/app/src/main/java/com/capstone/agrapanaapp/view/authentication/login/LoginActivity.kt
+++ MobileDevelopment/app/app/src/main/java/com/capstone/agrapanaapp/view/authentication/login/LoginActivity.kt
@@ -1,12 +1,21 @@
 package com.capstone.agrapanaapp.view.authentication.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.capstone.agrapanaapp.R
+import com.capstone.agrapanaapp.databinding.ActivityLoginBinding
+import com.capstone.agrapanaapp.view.authentication.register.RegisterActivity
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+        binding.btnRegister.setOnClickListener {
+            startActivity(Intent(this@LoginActivity,RegisterActivity::class.java))
+        }
     }
 }
--- MobileDevelopment/app/app/src/main/java/com/capstone/agrapanaapp/view/authentication/register/RegisterActivity.kt
+++ MobileDevelopment/app/app/src/main/java/com/capstone/agrapanaapp/view/authentication/register/RegisterActivity.kt
@@ -1,4 +1,16 @@
 package com.capstone.agrapanaapp.view.authentication.register
 
-class RegisterActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.agrapanaapp.R
+import com.capstone.agrapanaapp.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+    }
 }
