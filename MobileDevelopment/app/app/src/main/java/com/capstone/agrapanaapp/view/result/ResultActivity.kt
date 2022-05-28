2d33c361f4c23a6a6d376eaf6ba52b2cf3433eef
Catfish-Emperor <muhild16@gmail.com>
2022-05-28 12:30:34 +0700

    Writing CameraActivity business code and adding ResultActivity

--- MobileDevelopment/app/app/src/main/java/com/capstone/agrapanaapp/view/result/ResultActivity.kt
+++ MobileDevelopment/app/app/src/main/java/com/capstone/agrapanaapp/view/result/ResultActivity.kt
@@ -1,4 +1,21 @@
 package com.capstone.agrapanaapp.view.result
 
-class ResultActivity {
+import android.Manifest
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.agrapanaapp.R
+
+class ResultActivity : AppCompatActivity() {
+
+    companion object {
+        const val CAMERA_RESULT = 200
+
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
 }
