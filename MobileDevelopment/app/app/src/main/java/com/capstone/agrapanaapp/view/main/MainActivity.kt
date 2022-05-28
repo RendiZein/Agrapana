9e2d4c01462b5f4e2a7311de00f1e10de5e6e999
Syn57 <luthfi.n57@gmail.com>
2022-05-28 11:58:20 +0700

    btn camera inserted

--- MobileDevelopment/app/app/src/main/java/com/capstone/agrapanaapp/view/main/MainActivity.kt
+++ MobileDevelopment/app/app/src/main/java/com/capstone/agrapanaapp/view/main/MainActivity.kt
@@ -3,15 +3,27 @@ package com.capstone.agrapanaapp.view.main
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CancellationSignal
 import com.capstone.agrapanaapp.R
+import com.capstone.agrapanaapp.databinding.ActivityMainBinding
 import com.capstone.agrapanaapp.view.authentication.login.LoginActivity
+import com.capstone.agrapanaapp.view.camera.CameraActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val i = Intent(this@MainActivity, LoginActivity::class.java)
         startActivity(i)
         supportActionBar?.hide()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.btnCamera.setOnClickListener {
+            startActivity(Intent(this@MainActivity, CameraActivity::class.java))
+        }
+
     }
 }
